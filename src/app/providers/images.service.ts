bb1e9d9f1a96e698af2bae8a6c689b76c6ed14c0
rafszul <rafszul@gmail.com>
2017-03-12 05:16:57 +0000

    ImagesService

--- src/app/providers/images.service.ts
+++ src/app/providers/images.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+// import { Http } from '@angular/http';
 
 @Injectable()
 export class ImagesService {
 
-  constructor() { }
+  images: FirebaseListObservable<any[]>;
 
+  constructor(private af: AngularFire) {};
+
+  getImages() {
+    this.images = this.af.database.list('/images') as FirebaseListObservable<Images[]>;
+    return this.images;
+
+    // http.get('https://investin-art.firebaseio.com/.json').map(
+    //   (res) => res.json()
+    // );
+
+  }
 }
+interface Images {
+    id?: string;
+    img?: string;
+    imgsmall?: string;
+    imgsq?: string;
+    imgname?: string;
+    title?: string;
+    year?: string;
+    medium?: string;
+    size?: string;
+    name?: string;
+    price?: string;
+}
+
+
