863a0b1d7f8d6b81e2f723a87e9a24762429dc6e
rafszul <rafszul@gmail.com>
2017-03-12 05:15:56 +0000

    auth service clean up

--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 
 
 @Injectable()
+
 export class AuthService {
-  public messages: FirebaseListObservable<any>;
-  public users: FirebaseListObservable<any>;
+  // public messages: FirebaseListObservable<any>;
+  // public users: FirebaseListObservable<any>;
   public displayName: string;
   public email: string;
 
   constructor(public af: AngularFire) {
-    this.messages = this.af.database.list('messages');
+    // this.messages = this.af.database.list('messages');
   }
 
   loginWithGoogle() {
@@ -36,14 +37,14 @@ export class AuthService {
   //   return this.af.auth.logout();
   // }
 
-  sendMessage(text) {
-    var message = {
-      message: text,
-      displayName: this.displayName,
-      email: this.email,
-      timestamp: Date.now()
-    };
-    this.messages.push(message);
-  }
+  // sendMessage(text) {
+  //   var message = {
+  //     message: text,
+  //     displayName: this.displayName,
+  //     email: this.email,
+  //     timestamp: Date.now()
+  //   };
+  //   this.messages.push(message);
+  // }
 
 }
