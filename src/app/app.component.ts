ba2069551b7fb11d78c5a279876e738cfc937027
rafszul <rafszul@gmail.com>
2017-04-15 22:44:36 +0100

    md works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'investinart works!';
+  title = '- invest in ART - works!';
 }
