3d43d7dec08ead26b401845cd3a74bf109299632
rafszul <rafszul@gmail.com>
2017-04-17 09:10:54 +0100

    playing with dialog

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -5,14 +5,15 @@ import { MdDialogRef } from '@angular/material';
 @Component({
   selector: 'investinart-dialog',
   templateUrl: './dialog.component.html',
-    template: `
-        <p>{{ title }}</p>
-        <p>{{ message }}</p>
-        <button type="button" md-raised-button 
-            (click)="dialogRef.close(true)">OK</button>
-        <button type="button" md-button 
-            (click)="dialogRef.close()">Cancel</button>
-    `,
+    // template: `
+    //     <p>{{ title }}</p>
+    //     <p>{{ message }}</p>
+    //     <button type="button" md-raised-button 
+    //         (click)="dialogRef.close(true)">OK</button>
+    //     <button type="button" md-button 
+    //         (click)="dialogRef.close()">Cancel</button>
+    // `,
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent implements OnInit {
 
