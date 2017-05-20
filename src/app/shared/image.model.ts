c154c73999936f330dea1be200f7415d14b79787
rafszul <rafszul@gmail.com>
2017-05-20 14:13:50 +0600

    lets

---
+++ src/app/shared/image.model.ts
@@ -0,0 +1,13 @@
+export class Image {
+    id: number;
+    img: string;
+    imgsmall: string;
+    imgsq: string;
+    imgname: string;
+    title: string;
+    year: string;
+    medium: string;
+    size: string;
+    name: string;
+    price: string;
+}
