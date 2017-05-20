880c7b676b955880f658a44e14316cd9f3a60646
rafszul <rafszul@gmail.com>
2017-05-20 14:28:35 +0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ForCollectorsComponent } from './for-collectors/for-collectors.component';
+import { ForArtistsComponent } from './for-artists/for-artists.component';
+import { ForInvestorsComponent } from './for-investors/for-investors.component';
+import { HelloComponent } from './hello/hello.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TeamComponent } from './team/team.component';
+import { TandcComponent } from './tandc/tandc.component';
+
 const routes: Routes = [
+  { path: '', redirectTo: 'hello', pathMatch: 'full' },
+  { path: 'hello', component: HelloComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: TeamComponent },
+  { path: 'tandc', component: TandcComponent },
+  { path: 'collectors', component: ForCollectorsComponent },
+  { path: 'artists', component: ForArtistsComponent },
+  { path: 'investors', component: ForInvestorsComponent },
   {
     path: '',
     children: []
