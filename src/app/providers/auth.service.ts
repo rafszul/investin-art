3167d778b8f31a452266d668af3bf9a7fa36d30d
rafszul <rafszul@gmail.com>
2017-05-21 10:40:57 +0600

    Terms -> terms

---
+++ src/app/providers/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+
+
+@Injectable()
+
+export class AuthService {
+  // public messages: FirebaseListObservable<any>;
+  // public users: FirebaseListObservable<any>;
+  public displayName: string;
+  public email: string;
+
+  constructor(public af: AngularFire) {
+    // this.messages = this.af.database.list('messages');
+  }
+
+  loginWithGoogle() {
+    return this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    });
+  }
+  /**
+   * Logs out the current user
+   */
+  logout() {
+    return this.af.auth.logout();
+}
+}
